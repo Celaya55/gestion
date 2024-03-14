e4e63a83d9b2710dc931632ae785ddc73f2210ad
Celaya55 <158091587+Celaya55@users.noreply.github.com>
2024-03-13 17:43:03 -0700

    Add files via upload

--- gestion/js/scripts.js
+++ gestion/js/scripts.js
@@ -1,24 +1,24 @@
 // scripts.js
 
 function calcularHorasExtra() {
-    var horasExtraTrabajadas = parseFloat(document.getElementById("horasextra").value);
+    let horasExtraTrabajadas = parseFloat(document.getElementById("horasextra").value);
     const extra = 35;
-    var totalHorasExtra = horasExtraTrabajadas * extra;
+    let totalHorasExtra = horasExtraTrabajadas * extra;
 
     // Calcular el salario total con horas extra
-    var totalSalario = parseFloat(document.getElementById("resultado").innerText.split(": ")[1]);
-    var salarioConExtra = totalSalario + totalHorasExtra;
+    let totalSalario = parseFloat(document.getElementById("resultado").innerText.split(": ")[1]);
+    let salarioConExtra = totalSalario + totalHorasExtra;
 
     // Mostrar el resultado en el elemento HTML correspondiente
     document.getElementById("resultado1").innerText = "Salario por día (incluyendo horas extra): " + salarioConExtra;
 }
 
 function calcularSalario() {
-    var horasTrabajadas = parseFloat(document.getElementById("numero").value);
-    var comboBox = document.getElementById("puesto");
-    var puestoSeleccionado = comboBox.value;
+    let horasTrabajadas = parseFloat(document.getElementById("numero").value);
+    let comboBox = document.getElementById("puesto");
+    let puestoSeleccionado = comboBox.value;
 
-    var salarioHora;
+    let salarioHora;
 
     switch (puestoSeleccionado) {
         case "ingeniero":
@@ -39,7 +39,7 @@ function calcularSalario() {
     }
 
     // Calcular el salario total
-    var totalSalario = horasTrabajadas * salarioHora;
+    let totalSalario = horasTrabajadas * salarioHora;
 
     // Mostrar el resultado en el elemento HTML correspondiente
     document.getElementById("resultado").innerText = "Salario por día: " + totalSalario;
@@ -48,11 +48,11 @@ function calcularSalario() {
     calcularHorasExtra();
 }
 function calcularTotalSalarios() {
-    var filas = document.querySelectorAll('table tr');
-    var totalSalarios = 0;
+    let filas = document.querySelectorAll('table tr');
+    let totalSalarios = 0;
 
-    for (var i = 1; i < filas.length; i++) {
-      var salario = filas[i].querySelector('td:nth-child(4)').textContent;
+    for (let i = 1; i < filas.length; i++) {
+      let salario = filas[i].querySelector('td:nth-child(4)').textContent;
       salario = salario.replace('$', '').replace(',', '');
       totalSalarios += parseInt(salario);
     }
